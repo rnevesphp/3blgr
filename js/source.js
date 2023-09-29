169aea5af5d4434ef93ee138790c5203a52ce195
Romeu Neves <51343870+rnevesphp@users.noreply.github.com>
2023-09-29 14:40:18 +0200

    :+1: fixed directory

--- js/source.js
+++ js/source.js
@@ -2,3 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     var elems = document.querySelectorAll(".carousel");
     var instances = M.Carousel.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems);
+});
+
+/* M.toast({ html: 'I am a toast!' }) */
