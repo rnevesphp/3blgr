8510507305d38b0596db029a03cacd4c08ebeb15
Romeu Neves <51343870+rnevesphp@users.noreply.github.com>
2024-02-20 14:15:20 +0100

    updating catalogo JS

--- front/views/catalogo/scriptCatalogo.js
+++ front/views/catalogo/scriptCatalogo.js
@@ -27,48 +27,51 @@ const getCatalogo = async () => {
         cata.insertAdjacentHTML
         (
             'beforeend', `
+
+            <div class=""></div>
             <main>
                 <div class="filtros row">
-                        <div class="col s4 m6 l6">
-                            <div class="filtro-marca input-field">
-                                <select name="category" id="category">
-                                    <option value="" disabled selected>Choose your option</option>
-                                    <option value="Guess">Guess</option>
-                                    <option value="Moschino">Moschino</option>
-                                    <option value="Lacoste">Lacoste</option>
-                                    <option value="Calvin Klein">Calvin Klein</option>
-                                    <option value="Hugo Boss">Hugo Boss</option>
-                                    <option value="Superdry">Superdry</option>
-                                    <option value="Nike">Nike</option>
-                                    <option value="Adidas">Adidas</option>
-                                    <option value="Valentino">Valentino</option>
-                                    <option value="Tommy H">Tommy H</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="col s4 m6 l6">
-                            <div class="filtro-marca input-field">
-                                <select name="category" id="category">
-                                    <option value="" disabled selected>Choose your Brand</option>
-                                    <option value="t-shirt">t-shirt</option>
-                                    <option value="Jeans">Jeans</option>
-                                    <option value="SweatShirts">SweatShirts</option>
-                                    <option value="Tops">Tops</option>
-                                    <option value="Polo-shirts">Polo-shirts</option>
-                                    <option value="knitWear">knitWear</option>
-                                    <option value="Sweaters">Sweaters</option>
-                                    <option value="Trousers">Trousers</option>
-                                    <option value="Cardigans">Cardigans</option>
-                                    <option value="Shorts">Shorts</option>
-                                    <option value="Jackets&Coats">Jackets&Coats</option>
-                                    <option value="Suit">Suit</option>
-                                </select>
-                            </div>
+                    <div class="col s4 m6 l6">
+                        <div class="filtro-marca input-field">
+                            <select name="category" id="category">
+                                <option value="" disabled selected>Choose your option</option>
+                                <option value="Guess">Guess</option>
+                                <option value="Moschino">Moschino</option>
+                                <option value="Lacoste">Lacoste</option>
+                                <option value="Calvin Klein">Calvin Klein</option>
+                                <option value="Hugo Boss">Hugo Boss</option>
+                                <option value="Superdry">Superdry</option>
+                                <option value="Nike">Nike</option>
+                                <option value="Adidas">Adidas</option>
+                                <option value="Valentino">Valentino</option>
+                                <option value="Tommy H">Tommy H</option>
+                            </select>
                         </div>
                     </div>
-            
-                    <div class="content row">
+
+                    <div class="col s4 m6 l6">
+                        <div class="filtro-marca input-field">
+                            <select name="category" id="category">
+                                <option value="" disabled selected>Choose your Brand</option>
+                                <option value="t-shirt">t-shirt</option>
+                                <option value="Jeans">Jeans</option>
+                                <option value="SweatShirts">SweatShirts</option>
+                                <option value="Tops">Tops</option>
+                                <option value="Polo-shirts">Polo-shirts</option>
+                                <option value="knitWear">knitWear</option>
+                                <option value="Sweaters">Sweaters</option>
+                                <option value="Trousers">Trousers</option>
+                                <option value="Cardigans">Cardigans</option>
+                                <option value="Shorts">Shorts</option>
+                                <option value="Jackets&Coats">Jackets&Coats</option>
+                                <option value="Suit">Suit</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="content">
                         <div class="col s12 m6 l3">
                             <div class="card">
                                 <div class="card-image">
@@ -76,44 +79,15 @@ const getCatalogo = async () => {
                                 </div>
                                 <div class="card-content">
                                     <div class="product-info">
-                                        <span class="card-title title-prod">
-                                        Sudadera ${marca.nombre}</span>
-                                        <span class="prod-description">43€</span>
-                                        <span class="text-description">Descripcion del producto Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-                                        </span>
-                                    </div>
-                                    <div class="prod-variant">
-                                        <span class="title-variant">Color</span>
-                                        <div class="color-variant">
-                                            <ul>
-                                                <li class="variant-rounded"></li>
-                                                <li class="variant-rounded"></li>
-                                                <li class="variant-rounded"></li>
-                                                <li class="variant-rounded"></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div class="prod-sizes">
-                                        <span class="title-variant">Tamaño</span>
-                                        <div class="sizes">
-                                            <ul>
-                                                <li class="size-box">XS</li>
-                                                <li class="size-box">S</li>
-                                                <li class="size-box">M</li>
-                                                <li class="size-box">L</li>
-                                            </ul>
-                                        </div>
+                                        <span class="card-title title-prod">${marca.nombre}</span>
+                                        <span class="text-description">Descripcion del producto Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis</span>
                                     </div>
                                     <div class="prod-cta">
-                                        <div class="quantity-counter">
-                                            <i class="fa-solid fa-minus"></i>
-                                            <div class="contador">2</div>
-                                            <i class="fa-solid fa-plus"></i>
+                                        <div class="add-to-car">
+                                            <button class="btn">
+                                                <i class="fa-solid fa-cart-shopping"></i>
+                                            </button>
                                         </div>
-                                    <div class="add-to-car">
-                                        <button class="btn">
-                                            <i class="fa-solid fa-cart-shopping"></i>
-                                        </button>
                                     </div>
                                 </div>
                             </div>
