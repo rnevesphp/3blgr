0e22b0c4b616d2b3dc407d79c48d1e0613416862
Romeu Neves <51343870+rnevesphp@users.noreply.github.com>
2024-02-21 08:13:13 +0100

    teste para recuperar dadaos do banco

---
+++ back/database/config.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+const pool = new Pool(
+    {
+        user: "postgres",
+        host: "localhost",
+        database: "canoa_web_stock",
+        password: "1234",
+        port: 5432
+    }
+)
+
+module.exports = { pool }; 
--- front/views/catalogo/scriptCatalogo.js
+++ front/views/catalogo/scriptCatalogo.js
@@ -27,50 +27,7 @@ const getCatalogo = async () => {
         cata.insertAdjacentHTML
         (
             'beforeend', `
-
-            <div class=""></div>
-            <main>
-                <div class="filtros row">
-                    <div class="col s4 m6 l4">
-                        <div class="filtro-marca input-field">
-                            <select name="category" id="category">
-                                <option value="" disabled selected>Choose your option</option>
-                                <option value="Guess">Guess</option>
-                                <option value="Moschino">Moschino</option>
-                                <option value="Lacoste">Lacoste</option>
-                                <option value="Calvin Klein">Calvin Klein</option>
-                                <option value="Hugo Boss">Hugo Boss</option>
-                                <option value="Superdry">Superdry</option>
-                                <option value="Nike">Nike</option>
-                                <option value="Adidas">Adidas</option>
-                                <option value="Valentino">Valentino</option>
-                                <option value="Tommy H">Tommy H</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="col s4 m6 l6">
-                        <div class="filtro-marca input-field">
-                            <select name="category" id="category">
-                                <option value="" disabled selected>Choose your Brand</option>
-                                <option value="t-shirt">t-shirt</option>
-                                <option value="Jeans">Jeans</option>
-                                <option value="SweatShirts">SweatShirts</option>
-                                <option value="Tops">Tops</option>
-                                <option value="Polo-shirts">Polo-shirts</option>
-                                <option value="knitWear">knitWear</option>
-                                <option value="Sweaters">Sweaters</option>
-                                <option value="Trousers">Trousers</option>
-                                <option value="Cardigans">Cardigans</option>
-                                <option value="Shorts">Shorts</option>
-                                <option value="Jackets&Coats">Jackets&Coats</option>
-                                <option value="Suit">Suit</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
+                <div class="">
                     <div class="content">
                         <div class="col s12 m6 l3">
                             <div class="card">
@@ -94,7 +51,6 @@ const getCatalogo = async () => {
                         </div>
                     </div>
                 </div>
-            </main>
             `
         );
     });
